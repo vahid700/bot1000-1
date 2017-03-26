d34e6d8418b6e08265bd7eed40f9ed2160fa7406
erfan70 <erfaaaaaaaaan@gmail.com>
2017-03-26 07:15:55 +0430

    ghadimi

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -1190,10 +1190,63 @@ local function run(msg, matches)
 				channel_get_bots(receiver, callback_clean_bots, {msg = msg})
 			end
 		end
-	end
 
 	
-if matches[1] == 'help' and not is_owner(msg) then
+        if matches[1] == "muteuser" and is_momod(msg) then
+			local chat_id = msg.to.id
+			local hash = "mute_user"..chat_id
+			local user_id = ""
+			if type(msg.reply_id) ~= "nil" then
+				local receiver = get_receiver(msg)
+				local get_cmd = "mute_user"
+				muteuser = get_message(msg.reply_id, get_message_callback, {receiver = receiver, get_cmd = get_cmd, msg = msg})
+			elseif matches[1] == "muteuser" and matches[2] and string.match(matches[2], '^%d+$') then
+				local user_id = matches[2]
+				if is_muted_user(chat_id, user_id) then
+					unmute_user(chat_id, user_id)
+					savelog(msg.to.id, name_log.." ["..msg.from.id.."] حذف ["..user_id.."] از لیست کاربران ساکت شده")
+					return "["..user_id.."] از لیست ساکت شده ها حذف شد"
+				elseif is_owner(msg) then
+					mute_user(chat_id, user_id)
+					savelog(msg.to.id, name_log.." ["..msg.from.id.."] اضافه شد ["..user_id.."] به لیست کابران ساکت شده")
+					return "["..user_id.."] اضافه شد به لیست ساکت شده ها"
+				end
+			elseif matches[1] == "muteuser" and matches[2] and not string.match(matches[2], '^%d+$') then
+				local receiver = get_receiver(msg)
+				local get_cmd = "mute_user"
+				local username = matches[2]
+				local username = string.gsub(matches[2], '@', '')
+				resolve_username(username, callbackres, {receiver = receiver, get_cmd = get_cmd, msg=msg})
+			end
+		end
+
+		if matches[1] == "muteslist" and is_momod(msg) then
+			local chat_id = msg.to.id
+			if not has_mutes(chat_id) then
+				set_mutes(chat_id)
+				return mutes_list(chat_id)
+			end
+			savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested SuperGroup muteslist")
+			return mutes_list(chat_id)
+		end
+		if matches[1] == "mutelist" and is_momod(msg) then
+			local chat_id = msg.to.id
+			savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested SuperGroup mutelist")
+			return muted_user_list(chat_id)
+		end
+
+		if matches[1] == 'settings' and is_momod(msg) then
+			local target = msg.to.id
+			savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested SuperGroup settings ")
+			return show_supergroup_settingsmod(msg, target)
+		end
+
+		if matches[1] == 'rules' then
+			savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested group rules")
+			return get_rules(msg, data)
+		end
+
+		if matches[1] == 'help' and not is_owner(msg) then
 			text = "اگر سوالی داری از سازنده ام امپراطور @Erfan_herkuless_051  بپرس  ⚔"
 			reply_msg(msg.id, text, ok_cb, false)
 		elseif matches[1] == 'help' and is_owner(msg) then
@@ -1211,6 +1264,55 @@ if matches[1] == 'help' and not is_owner(msg) then
 			return ""
 		end
 		
+		if matches[1] == 'peer_id' and is_admin1(msg)then
+			text = msg.to.peer_id
+			reply_msg(msg.id, text, ok_cb, false)
+			post_large_msg(receiver, text)
+		end
+
+		if matches[1] == 'msg.to.id' and is_admin1(msg) then
+			text = msg.to.id
+			reply_msg(msg.id, text, ok_cb, false)
+			post_large_msg(receiver, text)
+		end
+
+		--Admin Join Service Message
+		if msg.service then
+		local action = msg.action.type
+			if action == 'chat_add_user_link' then
+				if is_owner2(msg.from.id) then
+					local receiver = get_receiver(msg)
+					local user = "user#id"..msg.from.id
+					savelog(msg.to.id, name_log.." Admin ["..msg.from.id.."] با لینک به گروه اضافه شد")
+					channel_set_admin(receiver, user, ok_cb, false)
+				end
+				if is_support(msg.from.id) and not is_owner2(msg.from.id) then
+					local receiver = get_receiver(msg)
+					local user = "user#id"..msg.from.id
+					savelog(msg.to.id, name_log.." Support member ["..msg.from.id.."] ضافه شد به گروه")
+					channel_set_mod(receiver, user, ok_cb, false)
+				end
+			end
+			if action == 'chat_add_user' then
+				if is_owner2(msg.action.user.id) then
+					local receiver = get_receiver(msg)
+					local user = "user#id"..msg.action.user.id
+					savelog(msg.to.id, name_log.." Admin ["..msg.action.user.id.."] اضافه شد به گروه به وسیله [ "..msg.from.id.." ]")
+					channel_set_admin(receiver, user, ok_cb, false)
+				end
+				if is_support(msg.action.user.id) and not is_owner2(msg.action.user.id) then
+					local receiver = get_receiver(msg)
+					local user = "user#id"..msg.action.user.id
+					savelog(msg.to.id, name_log.." Support member ["..msg.action.user.id.."] اضافه شد به گروه به وسیله [ "..msg.from.id.." ]")
+					channel_set_mod(receiver, user, ok_cb, false)
+				end
+			end
+		end
+		if matches[1] == 'msg.to.peer_id' then
+			post_large_msg(receiver, msg.to.peer_id)
+		end
+	end
+end
 
 local function pre_process(msg)
   if not msg.text and msg.media then
